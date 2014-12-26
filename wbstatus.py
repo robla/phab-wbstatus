7eada68b4c3c8584d5fba486482343de06095a4e
Rob Lanphier <robla@robla.net>
2014-12-25 20:54:54 -0800

    Basic workboard diff implemented

--- wbstatus.py
+++ wbstatus.py
@@ -54,7 +54,15 @@ def main():
     #generate_report()
     old_workboard = parse_workboard_html(dateutil.parser.parse("2014-12-22T0:00PST"))
     new_workboard = parse_workboard_html(dateutil.parser.parse("2014-12-23T0:00PST"))
-    print json.dumps(old_workboard, indent=4, sort_keys=True)
+    allkeys = list(set(old_workboard.keys()).union(new_workboard.keys()))
+    for key in allkeys:
+        oldvalue = old_workboard.get(key)
+        newvalue = new_workboard.get(key)
+        if oldvalue == newvalue:
+            print "  {0}: {1}".format(key, oldvalue)
+        else:
+            print "* {0}: {1}->{2}".format(key, oldvalue, newvalue)
+    #print json.dumps(old_workboard, indent=4, sort_keys=True)
     #print json.dumps(new_workboard, indent=4, sort_keys=True)
 
 main()
