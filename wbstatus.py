ddfee29e9dc41de2c80fdf669a0191d99c8685b0
Rob Lanphier <robla@robla.net>
2014-12-27 10:27:13 -0800

    PEP8ified

--- wbstatus.py
+++ wbstatus.py
@@ -11,24 +11,27 @@ import string
 MWCORETEAM_PHID = "PHID-PROJ-oft3zinwvih7bgdhpfgj"
 WORKBOARD_HTML_CACHE = '/home/robla/2014/phabworkboard-data/html'
 
+
 # Scrape the HTML for a Phabricator workboard, and return a simple dict
 # that represents the workboard.  The HTML is pre-retrieved via cron job
-# that snarfs the HTML as much as hourly.  This function accesses the 
+# that snarfs the HTML as much as hourly.  This function accesses the
 # cache via timestamp, which is rounded to the nearest hour in the file
 # name.
 def parse_workboard_html(wbtime):
     # File name will look something like workboard-2014-12-22T00.html,
     # which corresponds to midnight on 2014-12-22
-    htmlhandle = open(os.path.join(WORKBOARD_HTML_CACHE, 'workboard-{:%Y-%m-%dT%H%Z}.html'.format(wbtime)))
+    filename = 'workboard-{:%Y-%m-%dT%H%Z}.html'.format(wbtime)
+    htmlhandle = open(os.path.join(WORKBOARD_HTML_CACHE, filename))
     soup = BeautifulSoup(htmlhandle)
     columns = soup.find_all(class_="phui-workpanel-view")
     retval = {}
     for col in columns:
         state = col.find(class_="phui-action-header-title").strings.next()
-        objnames=col.find_all(class_="phui-object-item-objname")
+        objnames = col.find_all(class_="phui-object-item-objname")
         for objname in objnames:
-            retval[objname.string]=state
+            retval[objname.string] = state
     return retval
+
 
 # Take data structures representing the task states in two workboards,
 # and return a dict that contains the tasks for which the state changed
@@ -44,10 +47,12 @@ def get_workboard_diff(old_workboard, new_workboard):
             diff[key] = (oldvalue, newvalue)
     return diff
 
+
 def get_activity_for_tasks(phab, tasks):
-    tasknums = [ int(string.lstrip(x,"T")) for x in tasks ]
+    tasknums = [int(string.lstrip(x, "T")) for x in tasks]
     activity = phab.maniphest.gettasktransactions(ids=tasknums)
     return activity
+
 
 def main():
     phab = phabricator.Phabricator()
@@ -63,11 +68,12 @@ def main():
     for tasknum, taskfeed in activity.iteritems():
         for tact in taskfeed:
             if (tact["transactionType"] == "projectcolumn" and
-                tact["oldValue"]["projectPHID"] == MWCORETEAM_PHID):
+                    tact["oldValue"]["projectPHID"] == MWCORETEAM_PHID):
                 item = {}
                 item['timestamp'] = int(tact['dateCreated'])
                 if isinstance(tact['oldValue']['columnPHIDs'], dict):
-                    item['oldValue'] = tact['oldValue']['columnPHIDs'].values()[0]
+                    item['oldValue'] = tact['oldValue'][
+                        'columnPHIDs'].values()[0]
                     phids.add(item['oldValue'])
                 else:
                     item['oldValue'] = None
@@ -79,7 +85,8 @@ def main():
 
     phidquery = phab.phid.query(phids=list(phids))
     for move in columnmoves:
-        time = datetime.datetime.fromtimestamp(move['timestamp']).strftime("%Y-%m-%d %H:%M UTC")
+        time = datetime.datetime.fromtimestamp(
+            move['timestamp']).strftime("%Y-%m-%d %H:%M UTC")
         if move['oldValue']:
             oldcolumn = phidquery[move['oldValue']]['name']
         else:
@@ -89,4 +96,3 @@ def main():
         print "{0} {1} {2} {3}".format(time, oldcolumn, newcolumn, author)
 
 main()
-
