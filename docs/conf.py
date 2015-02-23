d6a2bdcc541217d58c7dfa17354cd6d9d30081f6
Bryan Davis <bd808@wikimedia.org>
2015-02-22 21:54:47 -0700

    Add tox support
    Add configuration to run py.test, flake8 and sphinx with tox.

---
+++ docs/conf.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+from datetime import date
+
+sys.path.insert(0, os.path.abspath('..'))
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+templates_path = ['_templates']
+source_suffix = '.rst'
+master_doc = 'index'
+project = u'Workboard Status'
+copyright = u'%s, Rob Lanphier and Wikimedia Foundation' % date.today().year
+version = '0.1'
+release = version
+exclude_patterns = ['_build']
+pygments_style = 'sphinx'
+html_theme = 'nature'
+html_static_path = ['_static']
+htmlhelp_basename = 'wbstatusdoc'
+
+autodoc_default_flags = ['members', 'private-members', 'special-members']
+autodoc_memeber_order = 'groupwise'
